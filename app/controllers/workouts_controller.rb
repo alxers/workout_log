57c9c93ad19fc2754f0cb72b0909428053a47a6a
Alexey Ershov <alex4rom@gmail.com>
2015-10-25 20:21:16 +0200

    Add workout controller, views

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,25 @@
+class WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+
+    @workout.save
+    redirect_to @workout
+  end
+
+  private
+
+    def workout_params
+      params.require(:workout).permit(:name, :length)
+    end
+end
