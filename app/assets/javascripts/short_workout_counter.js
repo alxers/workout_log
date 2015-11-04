3cc80410b60725a571ca18b98b445898913f1a29
Alexey Ershov <alex4rom@gmail.com>
2015-11-04 18:43:33 +0300

    Add poor man's dom ready for counter

--- app/assets/javascripts/short_workout_counter.js
+++
@@ -1,9 +0,0 @@
-// Counter
-(function() {
-    var count = 1;
-    var el = document.querySelector('.js-short-workout-counter');
-    setInterval(function() {
-        count = count + 1;
-        el.innerHTML = count;
-    }, 1000);
-})()
---
+++ app/assets/javascripts/workout_counter.js
@@ -0,0 +1,10 @@
+// Counter
+document.addEventListener('DOMContentLoaded', function() {
+    var count = 1;
+    var el = document.querySelector('.js-workout-counter');
+    setInterval(function() {
+    debugger;
+        count = count + 1;
+        el.innerHTML = count;
+    }, 1000);
+});
