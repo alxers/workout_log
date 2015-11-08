860585a6fbf973fb0aee8967c065ab86ec5e5053
Alexey Ershov <alex4rom@gmail.com>
2015-11-08 21:02:37 +0200

    Add exercise time

--- app/assets/javascripts/pages/workout_counter.js
+++ app/assets/javascripts/pages/workout_counter.js
@@ -1,10 +1,50 @@
 // Counter
-document.addEventListener('DOMContentLoaded', function() {
-    var count = 1;
-    var el = document.querySelector('.js-workout-counter');
-    setInterval(function() {
-    // debugger;
-        count = count + 1;
-        el.innerHTML = count;
-    }, 1000);
-});
+// TODO: refactor
+
+document.addEventListener('DOMContentLoaded', makeCounter);
+
+function restTime() {
+  return 30;
+}
+
+function exerciseTime() {
+  return 10;
+}
+
+function incrementer() {
+  var count = 1;
+  var sets = 0;
+  var isResting = false;
+
+  setInterval(function() {
+    count += 1;
+    if (isResting && count > restTime()) {
+      count = 0;
+      isResting = false;
+    }
+
+    if (count > exerciseTime()) {
+      count = 0;
+      sets += 1;
+      getSetsCounterEl().innerHTML = sets;
+      isResting = true;
+    }
+    getTimeCounterEl().innerHTML = count;
+  }, 1000);
+}
+
+function getEl(elClass) {
+  return document.querySelector(elClass);
+}
+
+function getTimeCounterEl() {
+  return getEl('.js-workout-counter__time');
+}
+
+function getSetsCounterEl() {
+  return getEl('.js-workout-counter__sets');
+}
+
+function makeCounter() {
+  incrementer();
+}
