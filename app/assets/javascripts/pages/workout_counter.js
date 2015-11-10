f15cb4bab510eaf5b09e246a5d49c31e9996b41f
Alexey Ershov <alex4rom@gmail.com>
2015-11-10 09:56:06 +0300

    Add resting, fix rest/exercise counter cycle

--- app/assets/javascripts/pages/workout_counter.js
+++ app/assets/javascripts/pages/workout_counter.js
@@ -11,6 +11,10 @@ function exerciseTime() {
   return 10;
 }
 
+function getRestText() {
+  return 'Resting';
+}
+
 function incrementer() {
   var count = 1;
   var sets = 0;
@@ -20,13 +24,15 @@ function incrementer() {
     count += 1;
     if (isResting && count > restTime()) {
       count = 0;
+      getTimeCounterTextEl().innerHTML = '';
       isResting = false;
     }
 
-    if (count > exerciseTime()) {
+    if (!isResting && count > exerciseTime()) {
       count = 0;
       sets += 1;
       getSetsCounterEl().innerHTML = sets;
+      getTimeCounterTextEl().innerHTML = getRestText();
       isResting = true;
     }
     getTimeCounterEl().innerHTML = count;
@@ -43,6 +49,10 @@ function getTimeCounterEl() {
 
 function getSetsCounterEl() {
   return getEl('.js-workout-counter__sets');
+}
+
+function getTimeCounterTextEl() {
+  return getEl('.js-workout-counter__time--text');
 }
 
 function makeCounter() {
