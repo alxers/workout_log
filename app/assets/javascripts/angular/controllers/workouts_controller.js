461f4040c26380ac66111ddc569efe157ba12280
Alexey Ershov <alex4rom@gmail.com>
2015-11-25 16:57:32 +0300

    Add users controller

--- app/assets/javascripts/angular/controllers/workouts_controller.js
+++ app/assets/javascripts/angular/controllers/workouts_controller.js
@@ -1,5 +1,3 @@
-// var workoutsController = angular.module('workoutsController', []);
-
 angular.module('WorkoutApp').controller('WorkoutsCtrl', ['$scope', 'WorkoutService','$routeParams',
   function($scope, WorkoutService, $routeParams) {
     $scope.workouts = WorkoutService.query();
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @users = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Signed up'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
+end
